5c7a5e7fc1198a18f5b5dedf76a929cd47111076
azzaantar95 <azzaantar95@gmail.com>
2025-01-04 17:51:55 +0200

    creat client model

---
+++ src/main/java/hibernate/model/client/Client.java
@@ -0,0 +1,59 @@
+package hibernate.model.client;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "client")
+public class Client {
+
+    @Id
+    @Column(name = "clientId")
+    private Long id;
+    @Column(name = "clientFullName")
+
+    private String fullName;
+    @Column(name = "clientAge")
+
+    private int age;
+    @Column(name = "clientAddress")
+
+    private String address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+
+}
